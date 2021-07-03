0c4d6accbc670c741ada3ef831d81ae80fd2c77b
Shikha Mishra <shikhamishra@cedcoss.com>
2021-07-03 20:47:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
-
+echo $error-severity
+echo "Hello $1"
 cd $GITHUB_WORKSPACE
 sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --error-severity=10 --warning-severity=8 $GITHUB_WORKSPACE -s $*"
