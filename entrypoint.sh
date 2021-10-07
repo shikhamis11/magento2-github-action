1c41a91c54aada58cc23ad6089a8b30d5f7dd087
Shikha Mishra <mshikha1103@gmail.com>
2021-10-07 16:36:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ echo $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
 sh -c "/root/.composer/vendor/bin/phpcs -i"
 
-sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=Magento2 /home/runner/work/cenia-pro-graphql/cenia-pro-graphql/app/"
+sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=Magento2 ./"
 
