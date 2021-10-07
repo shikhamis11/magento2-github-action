5af5fcc861bcf33e68408be83073032164588a41
Shikha Mishra <mshikha1103@gmail.com>
2021-10-07 16:25:12 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 echo "Error Severity $1"
 echo "Warning Severity $2"
 cd $GITHUB_WORKSPACE
+sh -c "/root/.composer/vendor/bin/phpcs -i"
+
 sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=Magento2 $GITHUB_WORKSPACE -s ./"
 
