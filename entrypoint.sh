da95a9c061a5021afe6eaef8266d8d09f762a7be
Shikha Mishra <mshikha1103@gmail.com>
2021-10-07 21:32:49 +0530

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo $GITHUB_WORKSPACE
 
 cd $GITHUB_WORKSPACE
 sh -c "/root/.composer/vendor/bin/phpcs -i"
-sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --error-severity=$1 --warning-severity=$2 $GITHUB_WORKSPACE/app/"
+sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --error-severity=$1 --warning-severity=$2 $GITHUB_WORKSPACE -s ./"
